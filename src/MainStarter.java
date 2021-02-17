eb1d9e1871340b7970fd9afc273189b468ad4d7c
Dzianis Stsepaniuk <d.stsepaniuk@godeltech.com>
2021-02-17 10:50:40 +0300

    add logic to override methods

---
+++ src/MainStarter.java
@@ -0,0 +1,52 @@
+import com.godeltech.module03_pt313.MultidimArray;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MainStarter {
+
+    public static void main(String[] args) {
+        //add Integer types;
+        MultidimArray<Integer> integers = new MultidimArray<>(new Integer[1][3]);
+        integers.setStatementXY(0, 2, 159);
+        System.out.println(integers.getStatementXY(0, 2));
+
+        //add String types;
+        MultidimArray<String> strings = new MultidimArray<>(new String[1][3]);
+        strings.setStatementXY(0, 1, "Hello!");
+        System.out.println(strings.getStatementXY(0, 1));
+
+        //add custom types;
+        MultidimArray<Person> people = new MultidimArray<>(new Person[1][2]);
+        people.setStatementXY(0, 0, new Person("Georg", 18));
+        for (int i = 0; i < people.size(); i++) {
+            for (int j = 0; j < people.size(); j++) {
+                System.out.println(people.getStatementXY(i,j));
+            }
+        }
+        //add try interface;
+        Collection<Integer> collection = new MultidimArray<>(new Integer[1][5]);
+        collection.add(456);
+        System.out.println(collection.toString());
+    }
+}
+
+
+class Person {
+    String name;
+    Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/com/godeltech/module03_pt313/MultidimArray.java
+++ src/com/godeltech/module03_pt313/MultidimArray.java
@@ -1,10 +1,21 @@
 package com.godeltech.module03_pt313;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class MultidimArray<T> implements Collection <T>{
+/**
+ * 1. Реализовать класс для работы с двумерным массивом на базе коллекций (интерфейса Collection).
+ * a) Реализовать методы get/set значения из строки и столбца
+ * b) При проходе по коллекции использовать итератор
+ * c) Все методы интерфейса Collection реализовывать не нужно, можно бросать UnsupportedOperationException
+ * 2. Создать аннотацию, которую можно поместить на поля класса, которая генерирует случайное значение в этом поле.
+ * Поля могут быть с типами: Integer, Boolean, String. Создать обработчик аннотации, который будет содержать логику генерации случайных значений.
+ */
 
-    T [][] array;
+public class MultidimArray<T> implements Collection<T> {
+    static Scanner scanner = new Scanner(System.in);
+    T[][] array;
+    T t;
 
     public MultidimArray(T[][] array) {
         this.array = array;
@@ -14,87 +25,91 @@ public class MultidimArray<T> implements Collection <T>{
         return array;
     }
 
-    public static void randomFillNumbers(Integer[][] array){
-        Random random = new Random();
-        for (int i = 0; i < array.length - 1; i++){
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = random.nextInt(100) + 1;
-            }
-        }
-
+    public T getStatementXY(Integer x, Integer y){
+        return this.array[x][y];
     }
 
-    public static void main(String[] args) {
-
-        MultidimArray<Integer> integers = new MultidimArray<>(new Integer[10][10]);
-        System.out.println(integers.getArray().length);
-        randomFillNumbers(integers.getArray());
-        System.out.println(Arrays.deepToString(integers.getArray()));
-
+    public void setStatementXY(Integer x, Integer y, T t){
+        this.array[x][y] = t;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.getArray().length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public Iterator<T> iterator() {
+
+
         return null;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public boolean add(T t) {
-        return false;
+        System.out.println("Input X " + (this.array.length - 1) + " maximum");
+        int x = scanner.nextInt();
+        System.out.println("Input X " + (this.array[x].length - 1) + " maximum");
+        int y = scanner.nextInt();
+        this.array[x][y] = t;
+        return t == null ? false : true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
+
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("Not support!");
     }
 
     @Override
     public void clear() {
+        throw new UnsupportedOperationException("Not support!");
+    }
 
+    @Override
+    public String toString() {
+        return "MultidimArray{" +
+                "array=" + Arrays.deepToString(array) +
+                '}';
     }
 }
