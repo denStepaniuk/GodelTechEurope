710297515335f897ffef7d6ce44202d4ee0e0860
Dzianis Stsepaniuk <d.stsepaniuk@godeltech.com>
2021-02-22 16:22:24 +0300

    remove folder "out" from cache

--- src/com/godeltech/module03_pt323/TicketsGiveaway.java
+++ src/com/godeltech/module03_pt323/TicketsGiveaway.java
@@ -1,5 +1,8 @@
 package com.godeltech.module03_pt323;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Сэмулировать работу по раздаче скидочных купонов на сайте магазина.
  * Число купонов(n) ограничено и устанавливается в начале работы магазина.
@@ -9,5 +12,43 @@ package com.godeltech.module03_pt323;
  */
 public class TicketsGiveaway {
 
+    public static void main(String[] args) {
+    ExecutorService exec = Executors.newFixedThreadPool(100);
+        for(int i = 0; i <= 100; i++){
+            exec.submit(new Costumer(i));
+        }
+    }
+}
 
+class Store {
+
+    int ticketQuantity;
+    int maxTicketPerCostumer;
+
+    public Store(int ticketQuantity, int maxTicketPerCostumer) {
+        this.ticketQuantity = ticketQuantity;
+        this.maxTicketPerCostumer = maxTicketPerCostumer;
+    }
+
+    public int getTicketQuantity() {
+        return ticketQuantity;
+    }
+
+    public int getMaxTicketPerCostumer() {
+        return maxTicketPerCostumer;
+    }
+}
+
+class Costumer implements Runnable {
+    int ticketOwn;
+    int costumerId;
+
+    public Costumer(int costumerId) {
+        this.costumerId = costumerId;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
