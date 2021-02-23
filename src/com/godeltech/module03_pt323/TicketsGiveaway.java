57206eaa90c016521cbcddb9177f6905c7cafc5a
Dzianis Stsepaniuk <d.stsepaniuk@godeltech.com>
2021-02-23 14:48:52 +0300

    TicketsGiveaway class done

--- src/com/godeltech/module03_pt323/TicketsGiveaway.java
+++ src/com/godeltech/module03_pt323/TicketsGiveaway.java
@@ -1,7 +1,11 @@
 package com.godeltech.module03_pt323;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Сэмулировать работу по раздаче скидочных купонов на сайте магазина.
@@ -11,44 +15,83 @@ import java.util.concurrent.Executors;
  * и суммарное число купонов у всех пользователей.
  */
 public class TicketsGiveaway {
+    static int quantityOfTickets = 1000;
+    static int quantityOfCostumers = 100;
 
-    public static void main(String[] args) {
-    ExecutorService exec = Executors.newFixedThreadPool(100);
-        for(int i = 0; i <= 100; i++){
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <= quantityOfTickets; i++) {
+            Store.getTicketList().add(new Ticket(i));
+        }
+
+        ExecutorService exec = Executors.newFixedThreadPool(quantityOfCostumers);
+        for (int i = 0; i <= quantityOfCostumers; i++) {
             exec.submit(new Costumer(i));
         }
+        exec.shutdown();
+        exec.awaitTermination(15, TimeUnit.SECONDS);
+        System.out.println("___________________________________________________________");
+        System.out.println("Total tickets costumers have: " + (quantityOfTickets - Store.getTicketList().size()));
+        System.out.println("Total costumers come: " + Costumer.costumers);
     }
 }
 
+/**
+ * Class realise store/keeper of tickets;
+ */
 class Store {
+    static List<Ticket> ticketList = new LinkedList<>();
+    static final int maxTicketPerCostumer = 10;
 
-    int ticketQuantity;
-    int maxTicketPerCostumer;
-
-    public Store(int ticketQuantity, int maxTicketPerCostumer) {
-        this.ticketQuantity = ticketQuantity;
-        this.maxTicketPerCostumer = maxTicketPerCostumer;
-    }
-
-    public int getTicketQuantity() {
-        return ticketQuantity;
-    }
-
-    public int getMaxTicketPerCostumer() {
-        return maxTicketPerCostumer;
+    public static List<Ticket> getTicketList() {
+        return ticketList;
     }
 }
 
+/**
+ * Class realise tickets for giveaway;
+ */
+class Ticket {
+    int ticketId;
+
+    public Ticket(int ticketId) {
+        this.ticketId = ticketId;
+    }
+}
+
+/**
+ * Class realise costumer which takes a tickets;
+ */
 class Costumer implements Runnable {
+    static int costumers;
     int ticketOwn;
     int costumerId;
+    final Object lock = new Object();
 
     public Costumer(int costumerId) {
         this.costumerId = costumerId;
+        costumers++;
+    }
+
+    void getTickets() {
+        Random random = new Random();
+        /**
+         * Random giveaway of tickets, no more than maxTicketPerCostumer count;
+         */
+        synchronized (lock) {
+            if (!Store.getTicketList().isEmpty()) {
+                for (int i = 0; i <= random.nextInt(Store.maxTicketPerCostumer); i++) {
+                    Store.getTicketList().remove(random.nextInt(Store.getTicketList().size() - 1));
+                    this.ticketOwn++;
+                }
+            } else {
+                return;
+            }
+            System.out.println("Costumer N:" + this.costumerId + " have " + this.ticketOwn + " tickets.");
+        }
     }
 
     @Override
     public void run() {
-
+        getTickets();
     }
 }
