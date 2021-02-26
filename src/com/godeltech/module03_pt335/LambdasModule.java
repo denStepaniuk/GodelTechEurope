eee8717d3dfbebd66a1bc7c2a650d1943954cb91
Dzianis Stsepaniuk <d.stsepaniuk@godeltech.com>
2021-02-26 16:57:22 +0300

    delete some not necessary logics

--- src/com/godeltech/module03_pt335/LambdasModule.java
+++ src/com/godeltech/module03_pt335/LambdasModule.java
@@ -3,9 +3,6 @@ package com.godeltech.module03_pt335;
 public class LambdasModule {
     public static void main(String[] args) {
 
-//        Summator result = (x, y) -> (x + y);
-//        System.out.println(result.sum(5, 5));
-
         System.out.println(summary(1, 2, (a, b) -> a + b));
     }
 
