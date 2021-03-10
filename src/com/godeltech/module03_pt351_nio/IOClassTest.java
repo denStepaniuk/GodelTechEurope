3dacfa0279b3c69732a4d6f7973d8f99f7339cd9
Denis Stepaniuk <72352969+denStepaniuk@users.noreply.github.com>
2021-03-10 12:42:35 +0300

    continue with ObjectIOStream

--- src/com/godeltech/module03_pt351_nio/IOClassTest.java
+++ src/com/godeltech/module03_pt351_nio/IOClassTest.java
@@ -1,10 +1,15 @@
 package com.godeltech.module03_pt351_nio;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Formatter;
 import java.util.concurrent.TimeUnit;
 
 /**
- *Try work with File class, readers/writers;
+ * Try work with File class, readers/writers;
  */
 
 public class IOClassTest {
@@ -12,21 +17,36 @@ public class IOClassTest {
     private static final File CREATE_FILE = new File(FOLDER_CREATOR + "\\someFile.txt");       //declare and initialise file name;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        FOLDER_CREATOR.mkdir();                                                      //create this ^ folder;
-        CREATE_FILE.createNewFile();                                                  //create this ^ file;
+        FOLDER_CREATOR.mkdir();                                                                         //create folder;
+        CREATE_FILE.createNewFile();                                                                    //create file;
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FOLDER_CREATOR + "\\someNewFile.txt"));
-             BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\d.stsepaniuk\\IdeaProjects\\GodelTechEurope\\src\\com\\godeltech\\module03_pt344\\Hrodna.txt"))) {
+             BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\d.stsepaniuk\\IdeaProjects" +
+                     "\\GodelTechEurope\\src\\com\\godeltech\\module03_pt344\\Hrodna.txt"))) {
             while (reader.readLine() != null) {
                 writer.write(reader.readLine() + "\n");
             }
         }
 
-        System.out.println("File create at directory: " + FOLDER_CREATOR.getPath() +"\nWith name: " + CREATE_FILE.getName());
-        TimeUnit.SECONDS.sleep(15);
-        CREATE_FILE.delete();                                                        //delete created file;
-        FOLDER_CREATOR.delete();                                                     //delete created folder;
+        System.out.println("File create at directory: " + FOLDER_CREATOR.getPath() + "\nWith name: " + CREATE_FILE.getName());
+        Path path = Paths.get(FOLDER_CREATOR + "someFile.txt");
+        System.out.println(Files.notExists(path));
 
+        TimeUnit.SECONDS.sleep(1);
+        CREATE_FILE.delete();                                                              //delete created file;
+        FOLDER_CREATOR.delete();                                                           //delete created folder;
+
+        /**
+         * Use Formatter class for write Strings into the file;
+         */
+        final BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\d.stsepaniuk\\IdeaProjects" +
+                "\\GodelTechEurope\\src\\com\\godeltech\\module03_pt344\\Hrodna.txt"));
+        final Formatter formatter = new Formatter("newFile.txt");
+
+        while(reader.ready()) {
+            formatter.format(reader.readLine() + "\n");
+        }
+        formatter.close();
     }
 
 }
